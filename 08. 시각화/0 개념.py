02976fb05be2c382020917560baccd9956030b5e
min2oyo <min2u@outlook.com>
2024-05-19 01:56:19 +0900

    07 딕셔너리

---
+++ 08. 시각화/0 개념.py
@@ -0,0 +1,13 @@
+#
+
+"""
+help()
+- 명령어, 모듈, 패키지, 라이브러리 등 정보 확인
+"""
+import random
+import matplotlib.pyplot
+
+# help(random)	// 길어서 주석 처리
+# help(random.randint)	// 길어서 주석 처리
+# help(matplotlib)	// 길어서 주석 처리
+# help(matplotlib.pyplot)	// 길어서 주석 처리
